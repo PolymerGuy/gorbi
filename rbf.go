47cdd235a7d943136992da003dd8cec5aaa98630
Sindre <sindre.n.olufsen@ntnu.no>
2019-05-05 11:17:33 +0200

    Broken for long series

--- rbf.go
+++ rbf.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/PolymerGuy/golmes/maths"
 	"gonum.org/v1/gonum/floats"
 	"gonum.org/v1/gonum/mat"
 	"math"
@@ -195,5 +196,21 @@ func main(){
 	fmt.Println(pdist(pas))
 	fmt.Println("Hypercube dims")
 	fmt.Println(hypercubeDims(pas))
+
+	args := [][]float64{{2.3},{2.9},{3.2},{2.6},{3.5},{3.8},{4.1},{3.6625}}
+	vals := []float64{0.49100787, 0.31960135, 0.24867926, 0.40236075, 0.20244296, 0.19892302,
+	0.24000631, 0.19443274}
+
+	rbf2 := NewRBF(args,vals)
+	newargs := maths.Linspace(2.3,3.6625,10)
+	NewArgs := [][]float64{}
+	NewArgs = append(NewArgs,newargs)
+
+	newvals := rbf2.ValuesAt(NewArgs)
+	fmt.Println(newvals)
+
+
+
+
 
 }
